86bc5b6a62f500b7c3ff30d3969e30191c8c9957
Phuc Tran Huu <phuc.th@neo-lab.com>
2020-04-22 22:27:28 +0700

    UI widget: AppBar

--- lib/app/modules/home/home_screen.dart
+++ lib/app/modules/home/home_screen.dart
@@ -6,10 +6,68 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  Icon btSearch = Icon(Icons.search);
+  Widget appBarTitle = Text('Home Page');
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      appBar: AppBar(
+        title: appBarTitle,
+        centerTitle: false,
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              colors: [
+                Color(0xFF832685),
+                Color(0xFFC81379),
+              ],
+            ),
+          ),
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: btSearch,
+            onPressed: () {
+              setState(() {
+                if (btSearch.icon == Icons.search) {
+                  btSearch = Icon(Icons.close);
+                  appBarTitle = TextField(
+                    autofocus: true,
+                    cursorColor: Color(0xFFAF2FB),
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      prefixIcon: Icon(
+                        Icons.search,
+                        color: Colors.white,
+                      ),
+                      filled: true,
+                      hintText: "Search...",
+                      hintStyle: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  );
+                } else {
+                  btSearch = Icon(Icons.search);
+                  appBarTitle = Text('Home Page');
+                }
+              });
+            },
+          ),
+        ],
+      ),
+      body: Container(
+        child: Center(
+          child: Text(
+            'Home Screen',
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/app/modules/onboarding/onboarding_screen.dart
+++ lib/app/modules/onboarding/onboarding_screen.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutterapp/app/routes/route_const.dart';
 
 class OnBoardingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: Container(
+        child: Center(
+          child: GestureDetector(
+            onTap: () {
+              Navigator.pushReplacementNamed(context, RouteName.HOME);
+            },
+            child: Text(
+              'OnBoarding Screen',
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
