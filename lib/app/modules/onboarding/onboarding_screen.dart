aaa76bdcf66764cd6e09bd24bdba187422a3d526
Phuc Tran Huu <phuc.th@neo-lab.com>
2020-04-26 00:10:30 +0700

    Add support language vn, en for project show case

--- lib/app/modules/onboarding/onboarding_screen.dart
+++ lib/app/modules/onboarding/onboarding_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutterapp/app/routes/route_const.dart';
+import 'package:flutterapp/app_localizations/app_localizations.dart';
 
 class OnBoardingScreen extends StatelessWidget {
   @override
@@ -12,7 +13,7 @@ class OnBoardingScreen extends StatelessWidget {
               Navigator.pushReplacementNamed(context, RouteName.HOME);
             },
             child: Text(
-              'OnBoarding Screen',
+              AppLocalizations.of(context).translate('hello'),
             ),
           ),
         ),
--- lib/application.dart
+++ lib/application.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'app/modules/splash/splash_screen.dart';
 import 'app/routes/routes_setting_ext.dart';
+import 'app_localizations/app_localizations.dart';
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -9,13 +11,39 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: SplashScreen(),
+      //Route
       onGenerateRoute: (RouteSettings settings) => settings.generateRoute,
       onUnknownRoute: (RouteSettings setting) {
         return MaterialPageRoute(builder: (context) => NotFoundPage());
       },
+      //Remove debug banner
       debugShowMaterialGrid: false,
       debugShowCheckedModeBanner: false,
       showSemanticsDebugger: false,
+      //Locale & Language
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        AppLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('en', 'US'), //English
+        const Locale('vi', 'VN'), //Vietnamese
+      ],
+      localeResolutionCallback: (locale, supportedLocales) {
+        //Check return locale default from setting of device.
+        print('Country code of device: ${locale.countryCode}');
+        for (var supportedLocale in supportedLocales) {
+          //If setting locale of device have in list locale support -> return this
+          if (supportedLocale.languageCode == locale.languageCode &&
+              supportedLocale.countryCode == locale.countryCode) {
+            return supportedLocale;
+          }
+        }
+        //Else return first locale from [list supportedLocales]
+        return supportedLocales.first;
+      },
     );
   }
 }
